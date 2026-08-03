cb07ff35c57672fabde96072f2dbcf2e883fa83c
Ankitlilhore <128494246+Ankitlilhore@users.noreply.github.com>
2026-08-03 17:10:33 +0530

    Update bronze_ingestion.py

--- bronze_ingestion.py
+++ bronze_ingestion.py
@@ -1,4 +1,29 @@
-
-
+#---------------------------------------------------------------------------------
+# Created scripts for dynamically ingest data from all the tables
+#---------------------------------------------------------------------------------
 
 entities = ['customers', 'trips', 'locations', 'payments', 'drivers', 'vehicles']
+
+for entity in entities:
+
+    batch_df  = ( spark.read.format("csv")
+                    .option("header", True)
+                    .option("inferschema", True)
+                    .load(f"/Volumes/pyspark_dbt/source/source_data/{entity}/")
+                )
+
+    batch_df_schema = batch_df.schema
+      
+    df = ( spark.readStream.format("csv")
+               .option("header", True)
+               .schema(batch_df_schema)
+               .load(f"/Volumes/pyspark_dbt/source/source_data/{entity}/")
+      
+      )
+    df.writeStream.format("delta")\
+              .outputMode("append")\
+              .option("checkpointLocation", f"/Volumes/pyspark_dbt/bronze/check_point/{entity}")\
+              .trigger(once=True)\
+              .toTable(f"pyspark_dbt.bronze.{entity}")
+
+
